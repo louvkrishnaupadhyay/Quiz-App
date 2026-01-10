b84c45763b2db533a8ea5c1bd9b56e7f008eda65
Louv <louv2k@gmail.com>
2026-01-10 15:31:14 +0530

    added the express validation

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -2,84 +2,97 @@ import type { NextFunction, Request, Response } from "express";
 import User from "../models/user.js";
 
 import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 import projectError from "../helper/error.js";
+import { validationResult } from "express-validator";
 
 interface returnResponse {
-    status: 'success' | 'error',
-    message: String,
-    data:{} | []
+  status: "success" | "error";
+  message: String;
+  data: {} | [];
 }
 
-
-const resisterUser = async (req: Request, res: Response, next:NextFunction) =>{
-
-    let resp:returnResponse;
-    try {
-        const email = req.body.email;
-        const name  = req.body.name;
-        
-        const password = await bcrypt.hash(req.body.password,12);
-
-
-        const user = new User({email, name, password});
-        const result = await user.save();
-        if(!result){
-            resp = {status: 'error', message: "result not found", data:{}};
-            res.send(resp);
-        }
-        else{
-            resp = {status: 'success', message: "Registration Done!", data:{userId:result._id}}
-            res.send(resp);
-        }
-    } catch (error) {
-        next(error);
+const resisterUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  let resp: returnResponse;
+  try {
+    const validationError = validationResult(req);
+    if (!validationError.isEmpty()) {
+      const err = new projectError("validation failed!");
+      err.statusCode = 422;
+      err.data = validationError.array();
+      throw err;
     }
-    
-    
-}
+    const email = req.body.email;
+    const name = req.body.name;
 
-const loginUser = async (req: Request, res: Response, next:NextFunction)=>{
+    const password = await bcrypt.hash(req.body.password, 12);
 
-    let resp:returnResponse;
-    try {
-
-        const email = req.body.email;
-        const password = req.body.password;
-
-        //find user with email
-        const user = await User.findOne({email});
-
-        if(!user){
-            const err = new projectError("User not found")
-            err.statusCode = 401;
-            throw err;
-        }
-
-
-        //verify password by bcrypt
-        const status = await bcrypt.compare(password, user.password);
-
-        //then decide
-        if(status){
-
-            const token = jwt.sign({userId:user._id}, "secret key", { expiresIn: '1h' });
-
-            resp = {status: 'success', message: "Logged In", data:{token}};
-            res.status(200).send(resp);
-        } 
-        else{
-            const err = new projectError("email/password mismatched")
-            err.statusCode = 401;
-            throw err;
-        }
-
-        
-        
-    } catch (error) {
-        next(error);
+    const user = new User({ email, name, password });
+    const result = await user.save();
+    if (!result) {
+      resp = { status: "error", message: "result not found", data: {} };
+      res.send(resp);
+    } else {
+      resp = {
+        status: "success",
+        message: "Registration Done!",
+        data: { userId: result._id },
+      };
+      res.send(resp);
     }
-}
+  } catch (error) {
+    next(error);
+  }
+};
 
+const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+  let resp: returnResponse;
+  try {
+    const email = req.body.email;
+    const password = req.body.password;
 
-export {resisterUser, loginUser};
+    //find user with email
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      const err = new projectError("User not found");
+      err.statusCode = 401;
+      throw err;
+    }
+
+    //verify password by bcrypt
+    const status = await bcrypt.compare(password, user.password);
+
+    //then decide
+    if (status) {
+      const token = jwt.sign({ userId: user._id }, "secret key", {
+        expiresIn: "1h",
+      });
+
+      resp = { status: "success", message: "Logged In", data: { token } };
+      res.status(200).send(resp);
+    } else {
+      const err = new projectError("email/password mismatched");
+      err.statusCode = 401;
+      throw err;
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+const isUserExist = async (email: String) => {
+  //find user with email
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return false;
+  }
+  return true;
+};
+
+export { resisterUser, loginUser, isUserExist };
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,13 +1,41 @@
 import express from "express";
-import {resisterUser, loginUser} from '../controllers/auth.js';
+import { resisterUser, loginUser, isUserExist } from "../controllers/auth.js";
+import { body } from "express-validator";
+import projectError from "../helper/error.js";
 
 const router = express.Router();
 
-
 // POST /auth/
-router.post('/', resisterUser);
+router.post(
+  "/",
+  [
+    body("name")
+      .trim()
+      .not()
+      .isEmpty()
+      .isLength({ min: 4 })
+      .withMessage("Please enter the valid name , minimum 4 character"),
+    body("email")
+      .trim()
+      .isEmail()
+      .custom((emailId) => {
+        return isUserExist(emailId)
+          .then((status) => {
+            if(status){
+                return Promise.reject("user already exist");
+            }
+          })
+          .catch((err) => {
+            return Promise.reject(err);
+          });
+      })
+
+      .normalizeEmail(),
+  ],
+  resisterUser
+);
 
 // POST /auth/login
-router.post('/login', loginUser)
+router.post("/login", loginUser);
 
 export default router;
